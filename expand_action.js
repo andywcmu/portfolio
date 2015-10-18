755704dbc6a0e74558ffcfd8babb8644c8a06b90
Andy Wang <eliowang@fb.com>
2015-10-17 23:19:08 -0400

    updated 10.15.2015

--- expand_action.js
+++ expand_action.js
@@ -1,5 +1,5 @@
 function show_project(id) {
-	console.log(document.getElementById(id).style);
+	// console.log(document.getElementById(id).style);
 	document.getElementById(id).style.maxHeight = "1000px";
 	
 	var buttonImage = document.getElementById(id + "_button_image");
@@ -8,14 +8,15 @@ function show_project(id) {
 	var buttonImageSrc = buttonImage.style.backgroundImage.replace(/url\((['"])?(.*?)\1\)/gi, '$2').split(',')[0];
 	var image = new Image();
     image.src = buttonImageSrc;
-    console.log(image.width, image.height);
+    // console.log(image.width, image.height);
 
     document.getElementById(id + "_button").style.height = 800.0 / image.width * image.height + "px";
 
 
 	document.getElementById(id + "_button_text").style.visibility = "hidden";
-	window.location.hash = "#" + id + "_button_image";
-	window.scrollBy(0,-80);
+	// document.getElementById(id + "_button_text").style.opacity = 0;
+	// window.location.hash = "#" + id + "_button_image";
+	// window.scrollBy(0,-80);
 }
 
 function jump_to_id(id) {
